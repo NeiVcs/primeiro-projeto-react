e3f1c4813fe6b845ae38b60ac524480cf65abd0b
NeiVcs <62248889+NeiVcs@users.noreply.github.com>
2022-01-31 15:37:10 -0300

    adicionei os forms

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from './components/pages/Home'
 import Contact from './components/pages/Contact'
 import Company from './components/pages/Company'
--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,6 +1,13 @@
+import ProjectForm from '../Project/ProjectForm'
+import styles from './NewProject.module.css'
+
 function NewProject(){
     return (
-        <p>continua..</p>
+        <div className={styles.newproject_container}>
+            <h1>Criar projeto</h1>
+            <p>Crie seu projeto para depois adicionar os serviços</p>
+            <ProjectForm btnText="Criar projeto"/>
+        </div>
     )
 }
 
